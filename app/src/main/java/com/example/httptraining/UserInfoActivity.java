785f4cde69754deb2e15d85de642113495223a9e
Kamal1292 <Kamal1292@users.noreply.github.com>
2020-04-13 11:14:08 +0600

    manifest setting

--- app/src/main/java/com/example/httptraining/UserInfoActivity.java
+++ app/src/main/java/com/example/httptraining/UserInfoActivity.java
@@ -1,6 +1,17 @@
 package com.example.httptraining;
 
-public class UserInfoActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
 
     public static final String USER_ID = "id";
 
